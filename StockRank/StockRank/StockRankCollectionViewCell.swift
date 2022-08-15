515230a279ab2c1b4bcd5c100ce83c3d16131527
KayAhn0126 <dkseotn123@gmail.com>
2022-08-15 19:53:21 +0900

    feat: StockRankCollectionViewCell 파일 추가

---
+++ StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by Kay on 2022/08/15.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
